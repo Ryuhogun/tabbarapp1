c0f8bd57043bdd56df33cec769f278b242ab8e37
Ryuhogun <fbghrjs0@naver.com>
2013-05-27 20:41:11 +0900

    5tabs

---
+++ Third.h
@@ -0,0 +1,13 @@
+//
+//  Third.h
+//  tabbarApp1
+//
+//  Created by 류 호건 on 13. 5. 27..
+//  Copyright (c) 2013년 류 호건. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Third : UIViewController
+
+@end
---
+++ Third.m
@@ -0,0 +1,38 @@
+//
+//  Third.m
+//  tabbarApp1
+//
+//  Created by 류 호건 on 13. 5. 27..
+//  Copyright (c) 2013년 류 호건. All rights reserved.
+//
+
+#import "Third.h"
+
+@interface Third ()
+
+@end
+
+@implementation Third
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
